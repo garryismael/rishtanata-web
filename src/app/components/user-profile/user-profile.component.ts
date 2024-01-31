244ff8f356d19c4f34ac5fb7cea6a3cc655edbf1
Garry <tahinjanaharygarry@gmail.com>
2024-01-31 18:38:43 +0300

    update profile

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,12 +1,18 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [NgOptimizedImage, MatProgressSpinnerModule, MatTabsModule],
+  imports: [
+    NgOptimizedImage,
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatDividerModule,
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css',
 })
